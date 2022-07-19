e8188c4025d74d4cc75f378f9cd05da2954a9029
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-07-19 18:09:52 +0900

    TIL: 컴포지트 패턴 학습

---
+++ src/main/java/designpattern/composite/Waitress.java
@@ -0,0 +1,13 @@
+package designpattern.composite;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
