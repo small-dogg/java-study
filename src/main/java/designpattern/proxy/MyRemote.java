278452526edc373b3a479211e9a76aa121f1310d
small-dogg <sy920112@naver.com>
2022-07-23 16:37:35 +0900

    TIL: 원격 프록시와 가상 프록시의 내용 정의 추가 기재

--- src/main/java/designpattern/proxy/MyRemote.java
+++ src/main/java/designpattern/proxy/MyRemote.java
@@ -3,6 +3,11 @@ package designpattern.proxy;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * 가상 프록시(Virtual Proxy)
+ * 실제 객체의 생성 비용이 비싸, 진짜로 실제 객체가 필요할 때 호출할 수 있도록
+ * 대리인이 대기하고잇다가 요청할때 호출.
+ */
 public interface MyRemote extends Remote {
     public String sayHello() throws RemoteException;
 }
--- src/main/java/designpattern/proxy/gumballmachine/GumballMachineRemote.java
+++ src/main/java/designpattern/proxy/gumballmachine/GumballMachineRemote.java
@@ -3,6 +3,9 @@ package designpattern.proxy.gumballmachine;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**원격 인터페이스(다른 JVM에 있는 객체를 대신 호출할 수 있도록 대리인의 역할을 수행)
+ * RMI(Remote Method Invocation) 프로토콜을 통해, 메시지를 전달함.
+ */
 public interface GumballMachineRemote extends Remote {
     int getCount() throws RemoteException;
 
