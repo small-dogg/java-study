9961e562b6a3b1d4347e1a0769e35dc6be57c9bc
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-09-25 13:54:42 +0900

    TIL: 코딩테스트 연습문제

---
+++ src/main/java/codingtest/Caldate.java
@@ -0,0 +1,47 @@
+package codingtest;
+
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Caldate {
+    public String solution(int a, int b) {
+        LocalDate date = LocalDate.of(2016,a,b);
+        return date.getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US);
+    }
+
+//    public String solution(int a, int b) {
+//        String answer = "";
+//
+//        int[] c = {31,29,31,30,31,30,31,31,30,31,30,31};
+//        String[] MM ={"FRI","SAT","SUN","MON","TUE","WED","THU"};
+//        int Adate = 0;
+//        for(int i = 0 ; i< a-1; i++){
+//            Adate += c[i];
+//        }
+//        Adate += b-1;
+//        answer = MM[Adate %7];
+//
+//        return answer;
+//    }
+
+//    public String getDayName(int a, int b) {
+//        String answer = "";
+//        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+//        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+//        int allDate = 0;
+//        for (int i = 0; i < a - 1; i++) {
+//            allDate += date[i];
+//        }
+//        allDate += (b - 1);
+//        answer = day[allDate % 7];
+//
+//        return answer;
+//    }
+
+
+
+    public static void main(String[] args) {
+        new Caldate().solution(4,15);
+    }
+}
