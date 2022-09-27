206be0a8dd8116c2ca9faf8ea45dfe733eae1a61
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-09-27 09:54:07 +0900

    TIL: 코딩테스트 연습문제(풀이중)

--- src/main/java/codingtest/KakaoFailRate.java
+++ src/main/java/codingtest/KakaoFailRate.java
@@ -8,30 +8,35 @@ public class KakaoFailRate {
 
         //스테이지별 실패 수
         Map<Integer,Integer> stageMap = new HashMap<>();
-        for(int i=0;i<stages.length;i++){
-            if (stages[i] == N + 1) {
+        for(int stage:stages){
+            if (stage == N + 1) {
                 continue;
             }
-            stageMap.put(stages[i], (stageMap.getOrDefault(stages[i], 0) + 1));
+            stageMap.put(stage, (stageMap.getOrDefault(stage, 0) + 1));
         }
 
         //스테이지별 실패 확률
         int amount = 0;
         Map<Integer,Double> resultMap = new HashMap<>();
         for (int i=1;i<N+1;i++) {
-            double failRate = (double)stageMap.getOrDefault(i,0)/(stages.length-amount);
-            amount+=stageMap.getOrDefault(i,0);
-            resultMap.put(i,failRate);
+            int failCount = stageMap.getOrDefault(i,0);
+            if(failCount!=0){
+                double failRate = (double)stageMap.getOrDefault(i,0)/(stages.length-amount);
+                amount+=stageMap.getOrDefault(i,0);
+                resultMap.put(i,failRate);
+            }else{
+                resultMap.put(i,0.0);
+            }
         }
 
         //확률 기준 정렬
         List<Map.Entry<Integer, Double>> list = new ArrayList<>(resultMap.entrySet());
         Collections.sort(list, (o1, o2) -> {
+            if(o1.getValue()==0&&o2.getValue()==0){
+                return o1.getKey()>o2.getKey()?1:-1;
+            }
             if(o1.getValue()==0) return 1;
             if(o2.getValue()==0) return -1;
-            if(o1.getValue()==o2.getValue()){
-                return -1;
-            }
             return o1.getValue()>o2.getValue()?-1:1;
         });
 
@@ -39,11 +44,11 @@ public class KakaoFailRate {
         for (int i = 0; i < N; i++) {
             answer[i]=list.get(i).getKey();
         }
-
+        System.out.println(Arrays.toString(answer));
         return answer;
     }
 
     public static void main(String[] args) {
-        new KakaoFailRate().solution(8,new int[]{1,2,3,4,5,6,7});
+        new KakaoFailRate().solution(13,new int[]{});
     }
 }
