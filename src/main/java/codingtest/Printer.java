53ff5729548cbd41e7da143e75e7e78283f6ab67
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-10-12 23:20:33 +0900

    TIL: 코테 연습문제 풀이

--- src/main/java/codingtest/Printer.java
+++ src/main/java/codingtest/Printer.java
@@ -1,23 +1,63 @@
 package codingtest;
 
+import java.util.*;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/42587
 public class Printer {
     public int solution(int[] priorities, int location) {
-        int max = 0;
-        int maxloc = 0;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+
         for (int i = 0; i < priorities.length; i++) {
-            if(priorities[i]>max){
-                max = priorities[i];
-                maxloc=i;
-            }
-        }
-        if(maxloc > location){
-           return maxloc-location + priorities.length;
-        }else if(maxloc < location){
-            return location - maxloc;
-        }else{
-            return 0;
+            queue.add(priorities[i]);
         }
 
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == queue.peek()) {
+                    if (i == location) {
+                        answer++;
+                        return answer;
+                    }
+                    queue.poll();
+                    answer++;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int solution2(int[] priorities, int location) {
+        int answer = 0;
+        int l = location;
+
+        Queue<Integer> que = new LinkedList<Integer>();
+        for(int i : priorities){
+            que.add(i);
+        }
+
+        Arrays.sort(priorities);
+        int size = priorities.length-1;
+
+
+
+        while(!que.isEmpty()){
+            Integer i = que.poll();
+            if(i == priorities[size - answer]){
+                answer++;
+                l--;
+                if(l <0)
+                    break;
+            }else{
+                que.add(i);
+                l--;
+                if(l<0)
+                    l=que.size()-1;
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
