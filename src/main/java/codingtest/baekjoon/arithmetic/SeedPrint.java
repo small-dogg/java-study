5de8959c549502eeaafe3d401cb87bc2c0f2325d
박선용 <74956749+small-dogg@users.noreply.github.com>
2022-10-26 10:44:53 +0900

    TIL: 코테 연습문제 풀이

---
+++ src/main/java/codingtest/baekjoon/arithmetic/SeedPrint.java
@@ -0,0 +1,14 @@
+package codingtest.baekjoon.arithmetic;
+
+//https://www.acmicpc.net/problem/25083
+
+public class SeedPrint {
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
