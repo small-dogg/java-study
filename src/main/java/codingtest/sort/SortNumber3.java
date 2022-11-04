bd7493f1d2164fad178a03f26feb522f84e589fd
small-dogg <sy920112@naver.com>
2022-11-04 23:48:09 +0900

    feat: 코딩테스트 문제 풀이

--- src/main/java/codingtest/sort/SortNumber3.java
+++ src/main/java/codingtest/sort/SortNumber3.java
@@ -3,26 +3,36 @@ package codingtest.sort;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+
+//Counting Sort
 
 public class SortNumber3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int length = Integer.parseInt(br.readLine());
 
-        List<Integer> dataList = new ArrayList<>();
-
+        int[] arr = new int[length];
+        int[] counting = new int[100001];
+        int[] result = new int[length];
         for (int i = 0; i < length; i++) {
-            dataList.add(Integer.parseInt(br.readLine()));
-
+            arr[i] = (Integer.parseInt(br.readLine()));
         }
 
-        Collections.sort(dataList);
+        for (int i : arr) {
+            counting[i]++;
+        }
+
+        for (int i = 1; i < counting.length; i++) {
+            counting[i]+=counting[i-1];
+        }
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            result[counting[arr[i]]-1]=arr[i];
+            counting[arr[i]]--;
+        }
 
         StringBuffer sb = new StringBuffer();
-        for(int value : dataList) {
+        for(int value : result) {
             sb.append(value).append('\n');
         }
         System.out.println(sb);
