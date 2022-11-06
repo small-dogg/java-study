ed9ed71516a5e2c9613e2c5cf1b1201bd6d40624
small-dogg <sy920112@naver.com>
2022-11-07 00:00:00 +0900

    feat: 코딩 테스트 문제 풀이

---
+++ src/main/java/codingtest/sort/AgeSort.java
@@ -0,0 +1,44 @@
+package codingtest.sort;
+
+//https://www.acmicpc.net/problem/10814
+
+import java.util.*;
+
+public class AgeSort {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        Person[] p = new Person[N];
+
+        for (int i = 0; i < N; i++)
+            p[i] = new Person(in.nextInt(), in.next());
+
+        Arrays.sort(p, Comparator.comparingInt(o -> o.age));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++)
+            sb.append(p[i]);
+
+        System.out.println(sb);
+
+
+    }
+
+    public static class Person {
+        private int age;
+        private String name;
+
+        public Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return age + " " + name + "\n";
+        }
+
+    }
+}
