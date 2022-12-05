8f1157a5d5557601145cd45d79a0095077942d57
small-dogg <sy920112@naver.com>
2022-12-05 20:22:35 +0900

    feat: 코딩 테스트 문제 풀이

---
+++ src/main/java/codingtest/baekjoon/backtracking/NandM3.java
@@ -0,0 +1,43 @@
+package codingtest.baekjoon.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM3 {
+    public static int m;
+    public static int n;
+
+    public static boolean[] visit;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int depth) {
+        if(depth == m){
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[depth] = i + 1;
+            dfs(depth+1);
+        }
+    }
+}
