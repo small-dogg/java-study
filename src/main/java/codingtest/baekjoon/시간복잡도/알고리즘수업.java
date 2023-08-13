aabffb6b2b40c459c53ebd596c3bf3b9bc5971e9
small-dogg <sy920112@naver.com>
2023-08-13 16:06:11 +0900

    feat: 코테 준비

---
+++ src/main/java/codingtest/baekjoon/시간복잡도/알고리즘수업.java
@@ -0,0 +1,16 @@
+package codingtest.baekjoon.시간복잡도;
+
+import java.util.Scanner;
+
+public class 알고리즘수업 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+
+    }
+
+    private int menOfPassion(int[] a, int n){
+        return a[a.length/n];
+    }
+}
---
+++ src/main/java/codingtest/basicmath1/JinBubChange.java
@@ -0,0 +1,12 @@
+package codingtest.basicmath1;
+
+import java.util.Scanner;
+
+public class JinBubChange {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Integer.valueOf(sc.next(), sc.nextInt()));
+    }
+}
