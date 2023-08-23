103ba693235ba422df860a6b084134968dc33a89
small-dogg <sy920112@naver.com>
2023-08-23 21:45:36 +0900

    feat: 문자열 압축 풀이 완료

--- src/main/java/codingtest/book/문자열/문자열압축.java
+++ src/main/java/codingtest/book/문자열/문자열압축.java
@@ -7,15 +7,16 @@ import java.util.StringTokenizer;
 public class 문자열압축 {
 
     public static void main(String[] args) {
-        System.out.println(new 문자열압축().solution("abcabcdede"));
+        System.out.println(new 문자열압축().solution("aabbaccc"));
     }
 
     public List<String> tokenize(String s, int length) {
+
         ArrayList<String> tokens = new ArrayList<>();
 
         for (int i = 0; i < s.length(); i += length) {
             int end = i + length;
-            if (s.length() < length) {
+            if (s.length() < end) {
                 end = s.length();
             }
             tokens.add(s.substring(i, end));
@@ -33,24 +34,30 @@ public class 문자열압축 {
         int size = 1;
 
         for (int i = 1; i < tokens.size(); i++) {
-            if (!tokens.get(i).equals(before)) {
+            String now = tokens.get(i);
+            if (!now.equals(before)) {
                 if(size>1) {
                     sb.append(size).append(before);
                 }else{
                     sb.append(before);
                 }
-                size = 0;
+                size = 1;
             } else {
                 size++;
             }
-            before = tokens.get(i);
+            before = now;
         }
+
+        if(size>1){
+            sb.append(size);
+        }
+        sb.append(before);
 
         return sb.toString().length();
     }
 
     public int solution(String s) {
-        int min = Integer.MAX_VALUE;
+        int min = s.length();
 
         for (int length = 1; length < s.length(); length++) {
             int compress = compress(s, length);
